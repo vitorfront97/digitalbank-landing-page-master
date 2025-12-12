2fbd0fde4fef54b0a9b364e727b43721b7712b7b
vitorfront97 <vitorfront97@gmail.com>
2025-12-12 08:26:17 -0300

    Tentativa 3 - solucionando problema de overflow no safari

--- app/widgets/Main.tsx
+++ app/widgets/Main.tsx
@@ -3,15 +3,17 @@ import Button from "../components/Button";
 
 export default function Main() {
   return (
-    <main className="min-w-0 pb-16 px-6 flex flex-col items-center bg-this-white bg-[url(/bg-intro-mobile.svg)] bg-top-right bg-no-repeat md:px-24 lg:bg-[url(/bg-intro-desktop.svg)] lg:bg-position-[top_-16rem_left_32rem] lg:flex-row-reverse lg:overflow-x-hidden xl:bg-position-[top_-16rem_left_38rem] 2xl:px-36 2xl:bg-position-[bottom_8rem_left_42rem]">
-      <Image
-        className="h-auto relative -top-29 lg:w-250 lg:-top-21 lg:-right-40 xl:-top-28 xl:-right-46"
-        src="/image-mockups.png"
-        width={375}
-        height={0}
-        alt="Celulares demonstrando a aplicação Digitalbank"
-      />
-      <div className="flex flex-col items-center gap-6 text-center lg:items-start lg:text-left xl:max-w-140">
+    <main className="pb-16 flex flex-col items-center bg-this-white bg-[url(/bg-intro-mobile.svg)] bg-top-right bg-no-repeat lg:bg-[url(/bg-intro-desktop.svg)] lg:bg-position-[top_-16rem_left_32rem] lg:flex-row-reverse xl:bg-position-[top_-16rem_left_30rem] 2xl:bg-position-[bottom_12rem_left_35rem]">
+      <div className="overflow-hidden">
+        <Image
+          className="h-auto relative -top-29 lg:w-250 lg:-top-21 lg:-right-16 xl:-top-37 xl:-right-28 2xl:-top-40 2xl:-right-32"
+          src="/image-mockups.png"
+          width={375}
+          height={0}
+          alt="Celulares demonstrando a aplicação Digitalbank"
+        />
+      </div>
+      <div className="px-6 md:px-24 2xl:px-36 flex flex-col items-center gap-6 text-center lg:items-start lg:text-left xl:max-w-160 2xl:max-w-200">
         <h1 className="text-4xl text-this-blue-950 font-extralight lg:text-5xl 2xl:text-6xl">
           Next generation digital banking
         </h1>
